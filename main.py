44dc73501d854f0f542f4370e0f348e4836bb740
brenocrepaldi <brenogaia2004@gmail.com>
2024-04-10 22:01:00 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from controllers.auth import Auth
+
+
+def main():
+    auth = Auth()
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, username, email, password) -> None:
+        self.username = username
+        self.email = email
+        self.password = password
