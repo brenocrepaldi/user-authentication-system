e380661b1df611227201255a7741d1e7934dcbf6
brenocrepaldi <brenogaia2004@gmail.com>
2024-04-11 07:55:15 -0300

    modify user class

--- models/user.py
+++ models/user.py
@@ -1,5 +1,8 @@
 class User:
     def __init__(self, username, email, password) -> None:
         self.username = username
-        self.email = email
+        self._email = email
         self.password = password
+
+    def __str__(self) -> str:
+        print(f"user: {self.username}, email: {self._email}")
