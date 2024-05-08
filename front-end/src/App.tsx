deec99de6535edba1d10d4d5850add26cf7e0434
brenocrepaldi <brenogaia2004@gmail.com>
2024-05-08 10:59:56 -0300

    get user credentials function

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,22 +1,68 @@
+import React, { useState } from "react";
 import { Input } from "./components/Input";
 
 export function App() {
+	// Estados para armazenar os valores dos inputs
+	const [userData, setUserData] = useState({
+		user: "",
+		email: "",
+		password: "",
+	});
+
+	// Função para lidar com a mudança nos inputs
+	const handleInputChange = (
+		event: React.ChangeEvent<HTMLInputElement>,
+		fieldName: string
+	) => {
+		const { value } = event.target;
+		setUserData((prevState) => ({
+			...prevState,
+			[fieldName]: value,
+		}));
+	};
+
+	// Função para lidar com o envio do formulário
+	const getUserCredentials = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		return userData;
+	};
+
 	return (
 		<main className="bg-zinc-900 h-screen flex flex-col justify-center items-center">
-			<div className="flex flex-col gap-4 justify-center items-center">
-				{/* Inputs */}
-				<div className="flex flex-col gap-5">
-					<Input name="User" />
-					<Input name="Email" />
-					<Input name="Password" />
-				</div>
+			<form onSubmit={getUserCredentials}>
+				<div className="flex flex-col gap-4 justify-center items-center">
+					{/* Inputs */}
+					<div className="flex flex-col gap-5">
+						<Input
+							name="User"
+							type="text"
+							value={userData.user}
+							onChange={(e) => handleInputChange(e, "user")}
+						/>
+						<Input
+							name="Email"
+							type="email"
+							value={userData.email}
+							onChange={(e) => handleInputChange(e, "email")}
+						/>
+						<Input
+							name="Password"
+							type="password"
+							value={userData.password}
+							onChange={(e) => handleInputChange(e, "password")}
+						/>
+					</div>
 
-				<div>
-					<button className="bg-blue-400 p-2 pt-1 pb-1 rounded-sm w-16">
-						Entrar
-					</button>
+					<div>
+						<button
+							type="submit"
+							className="bg-blue-400 p-2 pt-1 pb-1 rounded-sm w-16"
+						>
+							Entrar
+						</button>
+					</div>
 				</div>
-			</div>
+			</form>
 		</main>
 	);
 }
--- front-end/src/components/Input.tsx
+++ front-end/src/components/Input.tsx
@@ -1,15 +1,22 @@
+import React from "react";
+
 interface InputProps {
 	name: string;
+	type: string;
+	value: string;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ name }: InputProps) {
+export function Input({ name, type, value, onChange }: InputProps) {
 	return (
 		<div className="flex">
 			<div className="flex items-center justify-center text-zinc-300 text-ld w-1/3 p-3 bg-zinc-700 rounded-sm rounded-e-none">
 				{name}
 			</div>
 			<input
-				type="text"
+				type={type}
+				value={value}
+				onChange={onChange}
 				className="bg-zinc-600 focus:border-zinc-800 text-zinc-300 rounded-sm rounded-s-none w-400 focus:outline-none p-3 caret-zinc-300"
 			/>
 		</div>
